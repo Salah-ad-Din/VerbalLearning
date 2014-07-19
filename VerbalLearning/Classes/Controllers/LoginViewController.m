d4ee909978359facb1b7ee89de5b582fad702b4d
Raymond Lee <83002783@qq.com>
2014-07-19 13:28:18 +0800

    登录界面
    加入机构选项框界面，交互

--- VerbalLearning/Classes/Controllers/LoginViewController.m
+++ VerbalLearning/Classes/Controllers/LoginViewController.m
@@ -11,6 +11,8 @@
 @interface LoginViewController ()
 
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UIButton *orgSelectButton;
+@property (weak, nonatomic) IBOutlet UITableView *orgSelectTableView;
 
 @end
 
@@ -32,6 +34,8 @@
     [super viewDidLoad];
     
     [_loginButton.layer setCornerRadius:5.0];
+    _orgSelectTableView.layer.borderWidth = 1.0f;
+    _orgSelectTableView.layer.borderColor = [UIColor colorWithRed:0.78 green:0.78 blue:0.8 alpha:1].CGColor;
 
 }
 
@@ -44,6 +48,45 @@
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
     return UIStatusBarStyleLightContent;
+}
+
+- (IBAction)orgSelectButtonPressed:(id)sender {
+    [self orgSelectTableViewAnimation];
+}
+
+- (void)orgSelectTableViewAnimation
+{
+    [UIView animateWithDuration:0.3 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        if (_orgSelectTableView.frame.size.height > 100) {
+            CGRect rect = _orgSelectTableView.frame;
+            rect.size.height = 0.0f;
+            _orgSelectTableView.frame = rect;
+        } else {
+            CGRect rect = _orgSelectTableView.frame;
+            rect.size.height = 193.0f;
+            _orgSelectTableView.frame = rect;
+        }
+    } completion:^(BOOL finished) {
+    
+    }];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"OrgSelectCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+
+    return cell;
 }
 
 @end
